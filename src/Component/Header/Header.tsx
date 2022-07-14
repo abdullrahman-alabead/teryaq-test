29a365654daf4ca6f979209e08b23a4d642b1ebe
Abdullrahman <white.nolan33@gmail.com>
2022-07-14 20:22:22 +0300

    fixed header link clicking bug, added variables

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -2,15 +2,23 @@ import './Header.scss'
 
 
 let Header = () => {
-
-
+  // li clicking fix
+  function liClick(ele: EventTarget){
+    if(ele instanceof HTMLElement){
+      let a = ele.firstElementChild
+      if(a instanceof HTMLElement){
+        a.click();
+      }
+    }
+  }
+  
   return (
     <div className='header d-flex justify-content-lg-end justify-content-sm-center align-items-center px-lg-5 px-sm-0'>
         <ul className='nav-links d-flex align-items-center justify-content-center'>
-          <li className='nav-link'><a href="#about">About Company</a></li>
-          <li className='nav-link'> <a href="#services">Services</a></li>
-          <li className='nav-link'> <a href="#clients"> Clients</a></li>
-          <li className='nav-link'> <a href="#contact"> Contact Us</a></li>
+          <li className='nav-link' onClick={(e) => {liClick(e.target)}}> <a href="#about">About Company</a></li>
+          <li className='nav-link' onClick={(e) => {liClick(e.target)}}> <a href="#services">Services</a></li>
+          <li className='nav-link' onClick={(e) => {liClick(e.target)}}> <a href="#clients"> Clients</a></li>
+          <li className='nav-link' onClick={(e) => {liClick(e.target)}}> <a href="#contact"> Contact Us</a></li>
         </ul>
       
     </div>
